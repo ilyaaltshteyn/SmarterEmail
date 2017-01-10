642d0ebcc18b231800f5f6357ec97257d2a0b7dd
ilya <ilyaaltshteyn@gmail.com>
2017-01-10 18:56:46 -0500

    bumped number of analyzed emails down

--- google_api_wrapper.py
+++ google_api_wrapper.py
@@ -105,7 +105,7 @@ class Gmail():
                 pass
 
         # Get messages for those ids:
-        self.message_ids = self.message_ids[:75] # snip for testing.
+        self.message_ids = self.message_ids[:20] # snip for testing.
 
         for m_id in self.message_ids:
             try:
