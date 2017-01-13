626e8704631098db4f24c31c25a66870ee29df7a
shamu92 <altshteyn@gmail.com>
2017-01-13 15:37:00 -0800

    readded metric content and js

--- static/main.js
+++ static/main.js
@@ -3,38 +3,36 @@
 
 function randomString(length) {
     return Math.round((Math.pow(36, length + 1) - Math.random() *
-    Math.pow(36, length))).toString(36).slice(1);
+        Math.pow(36, length))).toString(36).slice(1);
 };
 
-function createCookie(name,value,days) {
+function createCookie(name, value, days) {
     if (days) {
         var date = new Date();
-        date.setTime(date.getTime() + (days*24*60*60*1000));
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
         var expires = "; expires=" + date.toUTCString();
-    }
-    else var expires = "";
+    } else var expires = "";
     document.cookie = name + "=" + value + expires + "; path=/";
 };
 
 function readCookie(name) {
     var nameEQ = name + "=";
     var ca = document.cookie.split(';');
-    for(var i=0;i < ca.length;i++) {
+    for (var i = 0; i < ca.length; i++) {
         var c = ca[i];
-        while (c.charAt(0)==' ') c = c.substring(1,c.length);
-        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
     }
     return null;
 };
 
 function eraseCookie(name) {
-    createCookie(name,"",-1);
+    createCookie(name, "", -1);
 };
 
 // Set a cookie if there isn't already one set:
-if (readCookie('smartrEmailVisit')) {}
-else {
-  createCookie('smartrEmailVisit', randomString(15), 365);
+if (readCookie('smartrEmailVisit')) {} else {
+    createCookie('smartrEmailVisit', randomString(15), 365);
 };
 
 
@@ -46,32 +44,39 @@ $(document).ready(function() {
         event.preventDefault();
         $('.main-nav').animate({ height: 'toggle' });
     });
+    $('.home-button').on('click', function(event) {
+        event.preventDefault();
+        $('.home-button').css({ 'color': 'rgba(249,93,95,1)' })
+        $('.tools-button, .contact-button, .data-policy-button, .metrics-button').css({ 'color': 'black' })
+        $('.data-policy-content, .tools-content, .contact-content, .metrics-content').hide();
+        $('.home-content').show();
+    });
+    $('.metrics-button').on('click', function(event) {
+        event.preventDefault();
+        $('.metrics-button').css({ 'color': 'rgba(249,93,95,1)' })
+        $('.tools-button, .contact-button, .data-policy-button, .home-button').css({ 'color': 'black' })
+        $('.data-policy-content, .tools-content, .contact-content, .home-content').hide();
+        $('.metrics-content').show();
+    });
     $('.data-policy-button').on('click', function(event) {
         event.preventDefault();
-        $('.data-policy-button').css({'color':'rgba(249,93,95,1)'})
-        $('.tools-button, .contact-button, .home-button').css({'color':'black'})
-        $('.home-content, .tools-content, .contact-content').hide();
+        $('.data-policy-button').css({ 'color': 'rgba(249,93,95,1)' })
+        $('.tools-button, .contact-button, .home-button, .metrics-button').css({ 'color': 'black' })
+        $('.home-content, .tools-content, .contact-content, .metrics-content').hide();
         $('.data-policy-content').show();
     });
     $('.tools-button').on('click', function(event) {
         event.preventDefault();
-        $('.tools-button').css({'color':'rgba(249,93,95,1)'})
-        $('.data-policy-button, .contact-button, .home-button').css({'color':'black'})
-        $('.home-content, .data-policy-content, .contact-content').hide();
+        $('.tools-button').css({ 'color': 'rgba(249,93,95,1)' })
+        $('.data-policy-button, .contact-button, .home-button, .metrics-button').css({ 'color': 'black' })
+        $('.home-content, .data-policy-content, .contact-content, .metrics-content').hide();
         $('.tools-content').show();
     });
     $('.contact-button').on('click', function(event) {
         event.preventDefault();
-        $('.contact-button').css({'color':'rgba(249,93,95,1)'})
-        $('.tools-button, .data-policy-button, .home-button').css({'color':'black'})
-        $('.home-content, .tools-content, .data-policy-content').hide();
+        $('.contact-button').css({ 'color': 'rgba(249,93,95,1)' })
+        $('.tools-button, .data-policy-button, .home-button, .metrics-button').css({ 'color': 'black' })
+        $('.home-content, .tools-content, .data-policy-content, .metrics-content').hide();
         $('.contact-content').show();
-    });
-    $('.home-button').on('click', function(event) {
-        event.preventDefault();
-        $('.home-button').css({'color':'rgba(249,93,95,1)'})
-        $('.tools-button, .contact-button, .data-policy-button').css({'color':'black'})
-        $('.data-policy-content, .tools-content, .contact-content').hide();
-        $('.home-content').show();
     });
 });
